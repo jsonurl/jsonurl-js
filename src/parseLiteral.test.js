60cf88e968e7c408722dc16404150197529feeab
David MacCormack <35852337+dmaccormack@users.noreply.github.com>
2020-06-12 22:05:07 -0400

    chore: Split the test suite into multiple files
    With the rename of main.js it was a good time to also split
    main.test.js into logical chunks.

---
+++ src/parseLiteral.test.js
@@ -0,0 +1,157 @@
+/*
+  MIT License
+
+  Copyright (c) 2020 David MacCormack
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+import JsonURL from "./JsonURL.js";
+
+function encodedString(s) {
+  return s
+    .replace(/'/, "%27")
+    .replace(/ /, "+")
+    .replace(/\(/, "%28")
+    .replace(/\)/, "%29")
+    .replace(/,/, "%2C")
+    .replace(/:/, "%3A");
+}
+
+function runTest(text, value, keyValue) {
+  expect(u.parseLiteral(text)).toBe(value);
+  expect(u.parseLiteral(text, 0, text.length, true)).toBe(keyValue);
+
+  //
+  // verify that parseLiteral() and parse() return the same thing (as
+  // expected for literals).
+  //
+  expect(u.parse(text)).toBe(value);
+}
+
+//
+// JsonURL.parseLiteral tests
+//
+const u = new JsonURL();
+
+test.each([
+  //
+  // boolean
+  //
+  true,
+  false,
+
+  //
+  // fixed point
+  //
+  0,
+  1,
+  -3,
+  1e2,
+  123456,
+  -123456,
+  12345678905432132,
+  -12345678905432132,
+  4e17,
+
+  //
+  // floating point
+  //
+  0.1,
+  2.0,
+  -3.14159,
+  -1.1,
+  1e-2,
+  -2e-1,
+  1.9e2,
+  -2.8e1,
+  156.9e-2,
+  -276.8e-1,
+  156.911e2,
+  -276.833e4,
+
+  //
+  // string
+  //,
+  "hello",
+  "a",
+  "1a",
+  "1e",
+  "1.",
+  "f",
+  "fa",
+  "fal",
+  "fals",
+  "n",
+  "nu",
+  "nul",
+  "t",
+  "tr",
+  "tru",
+  "-",
+  "-e",
+  "-.",
+  "1.2.3",
+  "hello, world",
+  "Bob's House",
+  "Hello, World!",
+  "World: Hello!",
+  "Hello (world).",
+])("JsonURL.parseLiteral(%p)", (value) => {
+  let keyValue = String(value);
+  let text = typeof value === "string" ? encodedString(keyValue) : keyValue;
+  runTest(text, value, keyValue);
+});
+
+test.each([
+  //
+  // fixed point
+  //
+  ["-3e0", -3],
+  ["1e+2", 1e2],
+  ["-2e+1", -2e1],
+
+  //
+  // floating point
+  //
+  ["156.911e+2", 156.911e2],
+
+  //
+  // string
+  //
+  ["'hello'", "hello"],
+  ["hello%2Bworld", "hello+world"],
+  ["y+%3D+mx+%2B+b", "y = mx + b"],
+  ["a%3Db%26c%3Dd", "a=b&c=d"],
+  ["hello%F0%9F%8D%95world", "hello\uD83C\uDF55world"],
+  ["-e+", "-e "],
+  ["-e+1", "-e 1"],
+  ["1e%2B1", "1e+1"],
+])("JsonURL.parseLiteral(%p)", (text, value) => {
+  let keyValue = typeof value === "string" ? value : text;
+  runTest(text, value, keyValue);
+});
+
+test("JsonURL.parseLiteral('null')", () => {
+  expect(u.parseLiteral("null")).toBe(u.nullValue);
+});
+
+test("JsonURL.parseLiteral('null', true)", () => {
+  expect(u.parseLiteral("null", 0, 4, true)).toBe("null");
+});
