57f5f5643b8d97a9ff9966af92adda46d58463f2
David MacCormack <35852337+dmaccormack@users.noreply.github.com>
2020-10-19 21:09:59 -0400

    chore: Change version_snapshot.sh to update Sonar properties

--- script/version_snapshot.sh
+++ script/version_snapshot.sh
@@ -2,8 +2,12 @@
 
 PACKAGE_VERSION=`sed -ne 's/^version=//p' snapshot.properties`
 PACKAGE_JSON=package.json
+SONAR_PROPERTIES=sonar-project.properties
 
 sed -i.bak "s/^\(\s*\"version\":\s*\"\)1.0.0-SNAPSHOT\"/\1$PACKAGE_VERSION\"/" "$PACKAGE_JSON"
+sed -i.bak "s/^\(\s*sonar.projectVersion\s*=\s*\)1.0.0-SNAPSHOT/\1$PACKAGE_VERSION/" "$SONAR_PROPERTIES"
+
 
 sed -n '/version/p' "$PACKAGE_JSON"
+sed -n '/sonar.projectVersion/p' "$SONAR_PROPERTIES"
 
